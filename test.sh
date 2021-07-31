e89eea3bf932e38c27d294fa11415688d6654172
ajay-navaneethan <nrajay006@gmail.com>
2021-07-31 03:42:50 -0700

    second commit v1.0

--- test.sh
+++ test.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
-if ["$1" == ""]
-then 
+if [ "$1" == "" ]
+then
 echo "You forgot an IP address!"
 echo "Syntax: ./file.sh <ip>"
 
 else
-nmap -p 80 -T5 $1
-echo "Completed successfully"
+ for ports in 80 443; do
+   nmap -p $ports -T5 $1 
+   echo "Completed successfully"
+   echo "-------------------------------------------------------------------------------------------------------------------------"
+  done
 fi
+
+
+#map -p 80 -T5 18.192.172.30 | grep tcp | cut -d " " -f 2
+
