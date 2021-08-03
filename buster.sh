6735d5d33efa24a7438e34cf1a3c56d10abc1e38
ajay-navaneethan <nrajay006@gmail.com>
2021-08-03 01:30:31 -0700

    v1.0

--- buster.sh
+++ buster.sh
@@ -1,28 +1,49 @@
 #!/bin/bash
+
+RED="\e[31m"
+ORANGE="\e[33m"
+BLUE="\e[94m"
+GREEN="\e[92m"
+STOP="\e[0m"
+WHITE="\e[37m"
+YELLOW = "\e[33m"
+
 if [ "$1" == "" ]
 then
+    printf "${WHITE}"
     echo "You forgot an IP address!"
     echo "Syntax: ./file.sh <ip>"
 
 else
+    printf "${GREEN}"
     figlet -c -f slant "buster"
-    for ports in 80 443; do
-        echo $ports
+   printf "${BLUE}"
+   echo "v1.0"
+   echo "by ajay"
+   echo "-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"
+   printf "${ORANGE}" 
+    echo "Starting NMAP full scan on background"
+    nmap -A -O -sV -sC $1 > nmap-full-scan.txt &
+    printf "${WHITE}"
+    for ports in 80 8080; do  #add ports that you want to enumerate here with a space
+	echo 
         nmap -p $ports -T5 $1 | grep open > status
         if [ -s status ]
         then
-           echo "file is not empty"
-           echo "starting gobuster"
-	   echo "starting nikto"
+	   printf "${GREEN}"
+           echo "Web server found !!!"
+	   echo
+	   printf "${ORANGE}"
+	   echo "Initialising NIKTO scan on background......"
+	   echo
+           echo "Starting Gobuster +++++++++++++++++++++++++++++++++++++++++++++++"
+	   printf "${WHITE}"
 	   nikto -h $1 -p $ports > nikto-$ports.txt &
+	   gobuster dir -u http://$1:$ports -w /usr/share/wordlists/dirb/common.txt -o gobuster-$ports.txt &
 	else
-	   echo "file is empty"
+	   echo "Web server not found on $PORT"
 	fi
-        echo "----------------------------------------------------------------------------------------"
     done
-# nikto -h $1 -p $ports > nikto-scan.txt &
 fi
 
-
-#map -p 80 -T5 18.192.172.30 | grep tcp | cut -d " " -f 2
 
